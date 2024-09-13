9b9fff19b6778d3fcea5c029d53e8f09b22ae3ee
sinamhdv <sinamhdv@gmail.com>
2024-09-13 16:05:46 -0700

    add basic vm code for if statements

--- src/vm/vm.c
+++ src/vm/vm.c
@@ -3,24 +3,36 @@
 #include "../utils/errors.h"
 #include <assert.h>
 
-static void vm_run_if_statement(AstNode *node) {
+SSValue vm_run_expression(AstNode *node) {
 
+}
+
+static void vm_run_if_statement(AstNode *node) {
+	DBGCHECK(node->type == AST_IF);
+	DBGCHECK(node->subs.size >= 2);
+	// int condition_val = vm_run_expression(node->subs.arr[0]);
+	// TODO: change the line above to return SSValue instead of int
+	if (condition_val != 0) {
+		vm_run_statement_list(node->subs.arr[1]);
+	} else if (node->subs.size == 3) {
+		vm_run_statement_list(node->subs.arr[2]);
+	}
 }
 
 static void vm_run_while_statement(AstNode *node) {
-
+	// TODO
 }
 
 static void vm_run_async_statement(AstNode *node) {
-
+	// TODO
 }
 
 static void vm_run_assignment_statement(AstNode *node) {
-	
+	// TODO
 }
 
 static void vm_run_expression_statement(AstNode *node) {
-
+	// TODO
 }
 
 static void vm_run_statement(AstNode *node) {
@@ -33,7 +45,7 @@ static void vm_run_statement(AstNode *node) {
 	}
 }
 
-static void vm_run_statement_list(AstNode *node) {
+void vm_run_statement_list(AstNode *node) {
 	DBGCHECK(node->type == AST_STATEMENT_LIST);
 	for (size_t i = 0; i < node->subs.size; i++) {
 		vm_run_statement(node->subs.arr[i]);
--- src/vm/vm.h
+++ src/vm/vm.h
@@ -3,6 +3,7 @@
 #ifndef MYHEADER_VM_H
 #define MYHEADER_VM_H
 
+void vm_run_statement_list(AstNode *node);
 void vm_main(AstNode *ast);
 
 #endif
