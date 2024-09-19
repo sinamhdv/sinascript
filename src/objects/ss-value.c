4adaa2e7785ed9ed75ddf71ac58551e9119a0854
sinamhdv <sinamhdv@gmail.com>
2024-09-18 21:16:37 -0700

    patch addition bin op to not make invalid types fixable

--- src/objects/ss-value.c
+++ src/objects/ss-value.c
@@ -17,20 +17,23 @@ int SSValue_cmp(SSValue a, SSValue b) {
 }
 
 SSValue SSValue_add(SSValue lval, SSValue rval, AstNode *error_location_node) {
-	if (lval.type != rval.type)
-		fatal_runtime_error(error_location_node);
 	SSValue result = {.type = lval.type};
-	switch (lval.type) {
-		case SSVALUE_NUM:
-			result.value = (void*)((SSNumber)lval.value + (SSNumber)rval.value);
-			break;
+	switch (rval.type) {
 		case SSVALUE_STR:
+			if (lval.type != SSVALUE_STR)
+				fatal_runtime_error(error_location_node);
 			result.value = SSString_concat(lval.value, rval.value);
 			break;
 		case SSVALUE_ARR:
+			if (lval.type != SSVALUE_ARR)
+				fatal_runtime_error(error_location_node);
 			result.value = SSArray_concat(lval.value, rval.value);
 			break;
-		default: fatal_runtime_error(error_location_node);
+		default:
+			if (lval.type != SSVALUE_NUM)
+				fatal_runtime_error(error_location_node);
+			result.value = (void*)((SSNumber)lval.value + (SSNumber)rval.value);
+			break;
 	}
 	return result;
 }
