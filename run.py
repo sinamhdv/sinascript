4a6d74c4e8d0d09744dc5b4526696ae4213855b0
sinamhdv <sinamhdv@gmail.com>
2024-09-19 02:59:53 -0700

    add run.py wrapper and change Makefile to compile stripped binary

--- run.py
+++ run.py
@@ -1,2 +1,23 @@
-#!/usr/bin/env python
-print("no run for now!")
+#!/usr/bin/env python3
+import subprocess
+import base64
+import tempfile
+
+try:
+	b64 = input("base64 input: ")
+	decoded = base64.b64decode(b64)
+
+	if (len(decoded) > 10240):
+		print("Too long!")
+		exit(1)
+
+	with tempfile.NamedTemporaryFile() as f:
+		f.write(decoded)
+		f.flush()
+		subprocess.check_call(["./sinascript", f.name])
+
+except:
+	print("Error!")
+	exit(1)
+
+
